c0f5a66e7655c7efaf3d8c75aaabd0c14f6f18da
algobardo <mezzetti@gmail.com>
2015-07-24 04:16:33 +0200

    Interceptor needs to override all the Object methods

--- sdk/lib/_internal/compiler/js_lib/interceptors.dart
+++ sdk/lib/_internal/compiler/js_lib/interceptors.dart
@@ -301,6 +301,10 @@ abstract class Interceptor {
   }
 
   Type get runtimeType => getRuntimeType(this);
+
+  int get customTag => Primitives.objectHashCode(this);
+
+  void setCustomTag(int tag) {}  
 }
 
 /**
